fe8b2027d2e3ca4d2cd1bb97393182e10dfaaba6
Joemhic <joemhic@outlook.com>
2023-02-21 17:49:56 -0800

    head

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main h"
+/**
+ * print_alphabet - Print lowercase alphabet
+ *
+ * Return: ALways 0;
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+		_putchar(a);
+
+	_putchar('\n');
+}
