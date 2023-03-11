5aae9964c664ecb435b79e515c1873ab38a7b55b
Joemhic <joemhic@outlook.com>
2023-03-10 16:11:53 -0800

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -28,15 +28,10 @@ int _atoi(char *s)
 	if (s[i] >= '0' && s[i] <= '9')
 	{
 	digit = s[i] - '0';
-
 	if (d % 2)
-
 	digit = -digit;
-
 	n = n * 10 + digit;
-
 	f = 1;
-
 	if (s[i + 1] < '0' || s[i + 1] > '9')
 
 	break;
@@ -49,7 +44,6 @@ int _atoi(char *s)
 	}
 
 	if (f == 0)
-
 	return (0);
 	return (n);
 }
