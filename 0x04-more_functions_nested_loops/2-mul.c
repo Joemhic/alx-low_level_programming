4d01a7c6f4b50aeb5f8829702a20ec3508c36024
Joemhic <joemhic@outlook.com>
2023-05-25 08:55:37 -0700

    more functions and nested loops -| `0-isupper.c` | Checs for uppercase character | | `1-isdigit.c` | Checks for a digit (`0` through `9`) | | `2-mul.c` | Multiplies two integers | | `3-print_numbers.c` | Prints the numbers, from `0` to `9`

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplication of 2 int
+ *
+ * @a: first int
+ *
+ * @b: second int
+ *
+ * Return: Results of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
